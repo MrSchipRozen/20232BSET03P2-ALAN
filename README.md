04549f663ead115c4745898b4cc62d3ef6cfde3e
MrSchipRozen <99350292+MrSchipRozen@users.noreply.github.com>
2023-12-15 15:19:18 -0300

    Commit Questao prova

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # 20232BSET03P2
+
 Inteli - Engenharia de Software | Avaliação 2023-2B P2
+
+Prova Alan Rozensztajn Schipper
+
+Vulnerabilidades Identificadas
+
+SQL Injection:
+-Vulnerabilidade a ataques de SQL Injection nas rotas de inserção de dados, como por exemplo as rotas de insercao de /cats estava com insercao direta de valores, o que torna vulneravel a SQL Injection
+
+Tratamento de Erros Inadequado:
+-Tratamento de erros insuficiente que poderia vazar detalhes da implementação.
+
+Lógica de Votação Insegura:
+-A votação estava sem verificação da existência do registro do animal, esta na parte de /vote/ :animalType:id , onde não verificarva se o animal existia antes de registrar o voto
+
+Métodos não estavam completos, como por exemplo o de:
+-As rotas GET e POST para /dogs estavam incompletas.
+
+O que eu implementei?
+
+-Inseri uma função que exige que o usuario bote um nome como uma verificação de seguranca "Insert name" e mostra um codigo de erro de 400 caso nao seja posto o nome, e melhora pois anteriormente estava duplicado as rotas post pra gatos e cachorros
+
+-Mudei a forma que as tabelas são criadas com o ID de Interger primaru key pra melhorar a seguranca e facilitar a identificacao e insercacao de regsitros sql
+
+-Melhorei a seguranca mudando a inserção de dados de interpolacao de string o que gera a vulnerabilidades do sql com a insercao sendo feito apenas com placeholders o que evita a injecao
+
+-Inseri uma funcao de tipo do animal onde verificar o tipo de animal e verificar se ele já existe atraves dos IDS
+
+-Na parte de sanitizacao utilizao "prepared statenebts" com paramentros na rota POST para /cats, o que faz a protecao contra o SQL injection de inserir novos registros de gatos
+
+-Fiz a implementação da rota de Post get para /dogs, que estavam incompletas, o que permite a insercao de novos registros de cachorros e a recuperacao de todos os registros de caes no banco de dados e usei a mesma logica de seguranca que apliquei na parte de gatos.
+
+-Funcao para que caso a pessao insira um gato ou cachorro com uma ID não existente ele retorno se foi um "gato" ou "cachorro" e a sua ID representando que ele nao existe.
--- index.js
+++ index.js
@@ -7,48 +7,75 @@ const port = 3000;
 
 app.use(bodyParser.json());
 
-
 const db = new sqlite3.Database(':memory:');
 
+//Inicializa a tabela
+
 db.serialize(() => {
-  db.run("CREATE TABLE cats (id INT, name TEXT, votes INT)");
-  db.run("CREATE TABLE dogs (id INT, name TEXT, votes INT)");
+  db.run("CREATE TABLE cats (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, votes INT)");
+  db.run("CREATE TABLE dogs (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, votes INT)");
 });
 
-app.post('/cats', (req, res) => {
-  const name = req.body.name;
-  db.run(`INSERT INTO cats (name, votes) VALUES ('${name}', 0)`, function(err) {
+function novoanimal(req, res, tableName) {
+  console.log
+  const { name } = req.body;
+  if (!name) {
+    return res.status(400).send("Nome é obrigatório");
+  }
+
+  db.run(`INSERT INTO ${tableName} (name, votes) VALUES (?, 0)`, [name], function(err) {
     if (err) {
-      res.status(500).send("Erro ao inserir no banco de dados");
-    } else {
-      res.status(201).json({ id: this.lastID, name, votes: 0 });
+      return res.status(500).send("Erro ao inserir no banco de dados");
     }
+    res.status(201).json({ id: this.lastID, name, votes: 0 });
   });
+}
+
+app.post('/cats', (req, res) => {
+  novoanimal(req, res, "cats");
 });
 
 app.post('/dogs', (req, res) => {
-  
+  novoanimal(req, res, "dogs");
 });
 
 app.post('/vote/:animalType/:id', (req, res) => {
- 
-  db.run(`UPDATE ${animalType} SET votes = votes + 1 WHERE id = ${id}`);
-  res.status(200).send("Voto computado");
-});
+  const { animalType, id } = req.params;
+  if (animalType !== 'cats' && animalType !== 'dogs') {
+    return res.status(400).send('Tipo de animal inválido');
+  }
 
-app.get('/cats', (req, res) => {
-  db.all("SELECT * FROM cats", [], (err, rows) => {
+  db.run(`UPDATE ${animalType} SET votes = votes + 1 WHERE id = ?`, [id], function(err) {
     if (err) {
-      res.status(500).send("Erro ao consultar o banco de dados");
-    } else {
-      res.json(rows);
+      return res.status(500).send("Erro ao atualizar o banco de dados");
     }
+    if (this.changes === 0) {
+      const animalName = animalType === 'cats' ? 'gato' : 'cachorro'
+      return res.status(404).send(`O ${animalName} com o ID de ${id} não foi encontrado`);
+    }
+    res.status(200).send("Voto computado");
   });
 });
 
+
+function todosAnimais(req, res, tableName) {
+  db.all(`SELECT * FROM ${tableName}`, [], (err, rows) => {
+    if (err) {
+      return res.status(500).send("Erro ao consultar o banco de dados");
+    }
+    res.json(rows);
+  });
+}
+
 app.get('/dogs', (req, res) => {
-  
+  todosAnimais(req, res, "dogs");
 });
+
+
+app.get('/cats', (req, res) => {
+  todosAnimais(req, res, "cats");
+});
+
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
